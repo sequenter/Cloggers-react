fb8b812de0555e061c4f950593a4c4052d0f0da0
sequenter <35529212+sequenter@users.noreply.github.com>
2025-08-18 22:15:56 +0100

    Added loading indicator

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,18 @@ import './App.css';
 
 import { CollectionLog, Featured, Footer, Header, Ranking, RecentItems } from '@components';
 
+import { useCollections } from '@hooks/useCollections';
+import { useGroup } from '@hooks/useGroup';
+import { useRecent } from '@hooks/useRecent';
 import { useSearch } from '@hooks/useSearch';
 
 import { useEffect } from 'react';
 
 function App() {
   const { setGroupId } = useSearch();
+  const { isLoading: collectionsLoading } = useCollections();
+  const { isLoading: groupLoading } = useGroup();
+  const { isLoading: recentLoading } = useRecent();
 
   useEffect(() => {
     const queryString = window.location.search;
@@ -21,6 +27,14 @@ function App() {
 
   return (
     <>
+      {(collectionsLoading || groupLoading || recentLoading) && (
+        <div className="absolute h-full">
+          <span className="sticky p-2 top-2 ml-2 text-shadow-none border-2 z-50 text-white border-white bg-black">
+            Loading - please wait.
+          </span>
+        </div>
+      )}
+
       <div className="flex flex-col gap-4 min-h-screen">
         <Header />
 
